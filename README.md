35549eec9cbdb5230cb5422a97a3832f7f992398
Alexander Berryhill-Williams <aberryhi@ucsd.edu>
2021-05-20 21:58:31 -0700

    Completed part 1

--- README.md
+++ README.md
@@ -3,11 +3,19 @@
 ## Check your understanding q's (FILL OUT)
 1. In your own words: Where would you fit your automated tests in your Bujo project development pipeline? (just write the letter)
 
-2. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+A
 
-3. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters
+1. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
 
-4. What do you expect to happen if we run our puppeteer tests with the field “headless” set to true?
+No, because this requires testing of writing and sending messages over the internet between two different users, which cannot be done within a single encapsulated component
 
-5. What would your beforeAll callback look like if you wanted to start from the settings page before every test case?
+1. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters
+
+Yes, because this action is probably handled by a single component and likely does not rely on other parts of the application, as it only requires checking the length of the message while it is being typed.
+
+1. What do you expect to happen if we run our puppeteer tests with the field “headless” set to true?
+
+
+
+2. What would your beforeAll callback look like if you wanted to start from the settings page before every test case?
 
--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,54 @@
  * @jest-environment jsdom
  */
 
+ import { pushToHistory } from '../scripts/router.js';
+
+describe('pushToHistory with settings state', () => {
+    test('increases length of history stack by 1', () => {
+        const histLength = history.length;
+        expect(pushToHistory('settings').length).toBe(histLength + 1);
+    });
+    test('sets current state object correctly', () => {
+        expect(pushToHistory('settings').state).toEqual({page: 'settings'});
+    });
+});
+
+describe('pushToHistory with entry state', () => {
+    describe('entry num 0', () => {
+        test('increases length of history stack by 1', () => {
+            const histLength = history.length;
+            expect(pushToHistory('entry',0).length).toBe(histLength + 1);
+        });
+        test('sets current state object correctly', () => {
+            expect(pushToHistory('entry',0).state).toEqual({page: 'entry0'});
+        });
+    });
+    describe('entry num 5', () => {
+        test('increases length of history stack by 1', () => {
+            const histLength = history.length;
+            expect(pushToHistory('entry',5).length).toBe(histLength + 1);
+        });
+        test('sets current state object correctly', () => {
+            expect(pushToHistory('entry',5).state).toEqual({page: 'entry5'});
+        });
+    });
+    describe('entry num 10', () => {
+        test('increases length of history stack by 1', () => {
+            const histLength = history.length;
+            expect(pushToHistory('entry',10).length).toBe(histLength + 1);
+        });
+        test('sets current state object correctly', () => {
+            expect(pushToHistory('entry',10).state).toEqual({page: 'entry10'});
+        });
+    });
+});
+
+describe('pushToHistory with other state', () => {
+    test('increases length of history stack by 1', () => {
+        const histLength = history.length;
+        expect(pushToHistory('other').length).toBe(histLength + 1);
+    });
+    test('sets current state object correctly', () => {
+        expect(pushToHistory('other').state).toEqual({});
+    });
+});
